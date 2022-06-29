8dcf5fd9dfbf23ce562098f3d0fe5024822d37f7
Ethan Painter <ethantpainter@gmail.com>
2022-06-29 16:25:50 -0400

    add card state filter variable to store and update search and layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
-	import '../app.css';
+	import "../app.css";
+	import Header from "$lib/header/Header.svelte";
+	import Search from "$lib/search/Search.svelte";
+	import StateCards from "$lib/cards/state-cards.svelte";
 </script>
 
-<main>
-	<slot />
-</main>
-
+<Header></Header>
+<Search customClasses="bg-gray-700 p-6"></Search>
+<div class="bg-gray-700">
+	<StateCards customClasses="grid grid-cols-3"></StateCards>
+</div>
