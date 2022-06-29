cb95bbbac6c970232762b7c868a73888edf97014
Ethan Painter <ethantpainter@gmail.com>
2022-06-29 16:50:24 -0400

    Move components to index instead of layout to support state specific pages

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,12 +1,7 @@
 <script lang="ts">
 	import "../app.css";
 	import Header from "$lib/header/Header.svelte";
-	import Search from "$lib/search/Search.svelte";
-	import StateCards from "$lib/cards/state-cards.svelte";
 </script>
 
-<Header></Header>
-<Search customClasses="bg-gray-700 p-6"></Search>
-<div class="bg-gray-700">
-	<StateCards customClasses="grid grid-cols-3"></StateCards>
-</div>
+<Header />
+<slot />
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,7 +2,17 @@
 	export const prerender = true;
 </script>
 
+<script lang="ts">
+	import Search from "$lib/search/Search.svelte";
+	import StateCards from "$lib/cards/state-cards.svelte";
+</script>
+
 <svelte:head>
 	<title>State Budget Search</title>
 	<meta name="State budget search" content="Budget search for U.S. states" />
 </svelte:head>
+
+<Search customClasses="bg-gray-700 p-6" />
+<div class="bg-gray-700">
+	<StateCards customClasses="grid grid-cols-3" />
+</div>
